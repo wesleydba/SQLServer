d7ed925854188c30f7cf4543b289942a8586fe72
Wesley Cardoso <53058749+wesleydba@users.noreply.github.com>
2024-12-18 15:03:34 -0300

    Update Estatistica verificar o status atual.sql

--- Performance SQL Server/Estatistica verificar o status atual.sql
+++ Performance SQL Server/Estatistica verificar o status atual.sql
@@ -1,16 +1,16 @@
 --Apenas Estatísticas que necessitam de atualização (Novo Filtro Rotina Administrativa - 10 dias)
-SELECT OBJECT_SCHEMA_NAME(obj.object_id) SchemaName, obj.name TableName,
-    stat.name, modification_counter,
-    [rows], rows_sampled, rows_sampled* 100 / [rows] AS [% Rows Sampled],
-    last_updated,
-    'UPDATE STATISTICS ' +OBJECT_SCHEMA_NAME(obj.object_id)+'.'+obj.name+'(['+ stat.name +'])' + ' WITH FULLSCAN'
-FROM sys.objects AS obj
-    INNER JOIN sys.stats AS stat ON stat.object_id = obj.object_id
+SELECT      OBJECT_SCHEMA_NAME(obj.object_id) SchemaName, obj.name TableName, 
+            stat.name, modification_counter, 
+            [rows], rows_sampled, rows_sampled* 100 / [rows] AS [% Rows Sampled],
+            last_updated, 
+			 'UPDATE STATISTICS ' +OBJECT_SCHEMA_NAME(obj.object_id)+'.'+obj.name+'(['+ stat.name +'])' + ' WITH FULLSCAN'
+FROM        sys.objects AS obj
+INNER JOIN  sys.stats AS stat ON stat.object_id = obj.object_id
 CROSS APPLY sys.dm_db_stats_properties(stat.object_id, stat.stats_id) AS sp
 WHERE       obj.is_ms_shipped = 0
-    AND (sp.modification_counter* 100 / [rows] >= 30 OR sp.rows_sampled* 100 / [rows] <= 70 OR DATEDIFF(DAY, sp.last_updated, GETDATE()) > 10)
-    AND obj.name not like '%_TTAT_LOG%' COLLATE Latin1_General_CI_AI
-ORDER BY    modification_counter DESC
+AND (sp.modification_counter* 100 / [rows] >= 30 OR sp.rows_sampled* 100 / [rows] <= 70 OR DATEDIFF(DAY, sp.last_updated, GETDATE()) > 10)
+AND obj.name not like '%_TTAT_LOG%' COLLATE Latin1_General_CI_AI
+ORDER BY    [rows] asc
 ---
 DECLARE @Command NVARCHAR(MAX)
 
